842b0acbf9f0e49322383c7f6c083175e9b448f1
maksr51314 <maksr51314@gmail.com>
2014-05-04 00:38:49 +0300

    [UI]Added simple menu

---
+++ views/js/controllers/menu/MyCtrl2.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.controllers', [])
+        .controller('MyCtrl1', [function() {
+            console.log('ssss')
+        }])
+
+
+}(window, navigator));
---
+++ views/js/controllers/menu/MyCtrl3.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp.controllers', [])
+        .controller('MyCtrl2', [function() {
+            console.log('ssss')
+        }])
+
+
+}(window, navigator));
